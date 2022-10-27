1d7834d05a27d38ceee3d3907ea125193025527d
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2022-10-27 15:33:41 -0400

    Update answers.js

--- problems/answers.js
+++ problems/answers.js
@@ -4,32 +4,32 @@ function greet() {
 }
 
 // Question 2
-function logOddNumbers() {
-  // Write your code here
-}
-
-// Question 3
 function area() {
   // Write your code here
 }
 
-// Question 4
+// Question 3
 function otherAngle() {
   // Write your code here
 }
 
-// Question 5
+// Question 4
 function diameterFromArea() {
   // Write your code here
 }
 
-// Question 6
+// Question 5
 function totalPrice() {
   // Write your code here
 }
 
-// Question 7
+// Question 6
 function shortLongShort() {
+  // Write your code here
+}
+
+// Question 7
+function stringy() {
   // Write your code here
 }
 
@@ -43,35 +43,17 @@ function isLeapYear() {
   // Write your code here
 }
 
-// Question 10
-function multisum() {
-  // Write your code here
-}
-
-// Question 11
-function asciiValue() {
-  // Write your code here
-}
-
-// Question 12
-function stringy() {
-  // Write your code here
-}
-
 // Do NOT touch or write anything below this line
   module.exports = {
     greet,
-    logOddNumbers,
     area,
     otherAngle,
     diameterFromArea,
     totalPrice,
     shortLongShort,
+    stringy,
     describeAge,
     isLeapYear,
-    multisum,
-    asciiValue,
-    stringy,
   };
 
 
